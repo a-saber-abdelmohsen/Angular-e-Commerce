3df6a0492b6c0cf3c106d97b7f47ab1294649dd3
a-saber-abdelmosen <76814982+a-saber-abdelmosen@users.noreply.github.com>
2021-07-26 11:48:31 +0200

    routing++

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< Updated upstream
 import { DetailsComponent } from './details/details.component';
-=======
 import { CartDetailsComponent } from './cart-details/cart-details.component';
->>>>>>> Stashed changes
 import { HomePageComponent } from './home-page/home-page.component';
 import { ProductListComponent } from './Store/product-list/product-list.component';
 import { WishlistDetailsComponent } from './wishlist-details/wishlist-details.component';
 
 const routes: Routes = [
-<<<<<<< Updated upstream
  {path:"details/:id",component:DetailsComponent},
-=======
 {path:"search/:id/:term", component:ProductListComponent},
 {path:"productList/:id",component:ProductListComponent}, 
->>>>>>> Stashed changes
 {path:"productList",component:ProductListComponent},
 {path:"homePage",component:HomePageComponent},
 {path:"Cart", component: CartDetailsComponent},
