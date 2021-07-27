31f099f47fddffd863eb7141dec0456bc4d658ca
salahsayed <slahs2452@gmail.com>
2021-07-27 23:39:47 +0200

    mainCategory

---
+++ src/app/Admin/MainCategory/add-main-category/add-main-category.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-main-category',
+  templateUrl: './add-main-category.component.html',
+  styleUrls: ['./add-main-category.component.scss']
+})
+export class AddMainCategoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,6 +27,12 @@ import { CheckOutComponent } from './check-out/check-out.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoaderComponent } from './header/loader/loader.component';
 import { LoaderInterceptor } from './loader/loader.interceptor';
+<<<<<<< Updated upstream
+=======
+import { IndexComponent } from './Admin/MainCategory/index/index.component';
+import { AddMainCategoryComponent } from './Admin/MainCategory/add-main-category/add-main-category.component';
+import { EditMainCategoryComponent } from './Admin/MainCategory/edit-main-category/edit-main-category.component';
+>>>>>>> Stashed changes
 
 
 @NgModule({
@@ -49,7 +55,14 @@ import { LoaderInterceptor } from './loader/loader.interceptor';
     CartComponent,
     CartProductComponent,
     CheckOutComponent,
+<<<<<<< Updated upstream
     LoaderComponent
+=======
+    LoaderComponent,
+    IndexComponent,
+    AddMainCategoryComponent,
+    EditMainCategoryComponent
+>>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
